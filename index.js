7ba0ee001a1f9afe9df397aaddb8360ceb981d72
Sohel-Developer <sohel.webdevoloper.com>
2023-07-29 16:06:08 +0600

    users saved and carts api create

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyhhy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,12 +30,55 @@ async function run() {
 
         /* Database Collections */
         const foodCollection = client.db("FoodRestorent").collection("foods");
+        const usersCollection = client.db("FoodRestorent").collection("users");
+        const cartCollection = client.db("FoodRestorent").collection("carts");
 
         /* Food Get Api */
         app.get('/foods', async (req, res) => {
             const result = await foodCollection.find().toArray()
             res.send(result)
         })
+
+        /* Users Saved */
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: 'user exists ' })
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+
+        // cart collection apis
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/carts', async (req, res) => {
+            const item = req.body;
+            const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
+
 
 
         // Send a ping to confirm a successful connection
