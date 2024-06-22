0c5d6c2d8476201d90766cc6ff3c250846ec2e6e
Sohel-Developer <sohel.webdevoloper@gmail.com>
2024-06-22 11:09:20 +0600

    Create Admin api

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
 var cors = require('cors')
+const jwt = require('jsonwebtoken');
 
 /* Midelware */
 
 app.use(express.json())
 app.use(cors())
+
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
@@ -33,6 +52,42 @@ async function run() {
         const usersCollection = client.db("FoodRestorent").collection("users");
         const cartCollection = client.db("FoodRestorent").collection("carts");
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token })
+        })
+
+        // Warning: use verifyJWT before using verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
+
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { Email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+
+
+
+
+
         /* Food Get Api */
         app.get('/foods', async (req, res) => {
             const result = await foodCollection.find().toArray()
@@ -57,6 +112,7 @@ async function run() {
             const result = await foodCollection.deleteOne(query);
             res.send(result);
         })
+
 
 
         /* Users Saved */
